0aba085a95741db65d9f7a94d98fd7a531a3c84b
Tyler Hawkins <thawkin3@byu.net>
2023-04-03 20:54:22 -0600

    fix: removes reportVitals.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import './index.css'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
